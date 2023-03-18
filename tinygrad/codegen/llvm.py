bef10fca33a78472eb2f65cfeb5c22762934019c
martinloretzzz <20306567+martinloretzzz@users.noreply.github.com>
2023-03-19 00:04:39 +0100

    Remove VectorType from llvm

--- tinygrad/codegen/llvm.py
+++ tinygrad/codegen/llvm.py
@@ -1,5 +1,5 @@
 import functools, math
-from typing import ClassVar, List
+from typing import ClassVar
 from llvmlite import ir  # type: ignore
 from tinygrad.codegen.ast import ASTKernel
 from tinygrad.ops import UnaryOps, BinaryOps, ReduceOps, LazyOp, ASTRunner
@@ -43,65 +43,10 @@ class LLVMCodegen(ASTKernel):
     self.process()
     if DEBUG >= 3: self.printbufs("old:", DEBUG>=4)
 
-    # this stuff can't be hand coded
-    kernel_output_axis: List[int] = []
-    """
-    CACHE_DIM = 32
-    if len(k.shapes[0]) == 2:
-      # cache tiling, makes permute fast
-      k.reshape_and_permute(
-        lambda shape: (shape[0]//CACHE_DIM, CACHE_DIM, shape[1]//CACHE_DIM, CACHE_DIM),
-        (0,2,1,3))
-    elif len(k.shapes[0]) == 3:
-      if k.reduceop:
-        if k.strides[1][-1] == 1 and k.strides[2][-1] == 1:
-          DY, DX = 8, 8
-        elif k.strides[1][-1] in [1,0] and k.strides[1][-2] in [1,0]:
-          DY, DX = 4, 16
-        else:
-          DY, DX = 16, 4
-        # matmul: YyXxK -> YXKyx
-        k.reshape_and_permute(
-          lambda shape: (shape[0]//DY, DY, shape[1]//DX, DX, shape[2]),
-          (0,2,4,1,3))
-        kernel_output_axis = [-2, -1]
-      else:
-        CACHE_L2_DIM = 256
-        k.reshape_and_permute(
-          lambda shape: (shape[0], shape[1]//CACHE_L2_DIM, CACHE_L2_DIM, shape[2]),
-          (1,0,2,3))
-        kernel_output_axis = [-1]
-    elif len(k.shapes[0]) == 7:
-      # conv: split chans and X
-      DY, DX = 4, 16
-      k.reshape_and_permute(
-        lambda shape: (shape[0], shape[1]//DY, DY, shape[2], shape[3]//DX, DX, shape[4], shape[5], shape[6]),
-        (0,1,3,4,6,7,8,2,5))
-      kernel_output_axis = [-2, -1]
-    """
-
     # the 4x4 need to go all the way at the end, even after reduce
     output_shape = self.sts[0].shape
     full_shape_options = [x.shape for x in self.sts if x.shape != output_shape]
     full_shape = output_shape if len(full_shape_options) == 0 else full_shape_options[0]
-
-    full_shape = full_shape if not kernel_output_axis else full_shape[:-len(kernel_output_axis)]
-    kernel_output_dim = prod([self.sts[0].shape[a] for a in kernel_output_axis])
-    kernel_output_type = ir.FloatType() if kernel_output_dim == 1 else ir.VectorType(ir.FloatType(), kernel_output_dim)
-
-    def get_idxs(builder, idx, buf_index):
-      idx_offsets = [0]
-      """
-      for axis in kernel_output_axis:
-        new_idx_offsets = []
-        for s in range(k.shapes[buf_index][axis]):
-          for i in idx_offsets:
-            new_idx_offsets.append(i + s * k.strides[buf_index][axis])
-        idx_offsets = new_idx_offsets
-      """
-      return [builder.add(idx, int_const(i)) for i in idx_offsets]
-
-    # *** llvm specific below this line ***
 
     # create llvm function
     module = ir.Module(name=__file__)
@@ -133,40 +78,26 @@ class LLVMCodegen(ASTKernel):
     # the ast parser
     def ast_parse(builder, x, level, reduce_result=None):
       if not isinstance(x, LazyOp):
-        m = kernel_output_type(ir.Undefined)
         buf_index = self.bufs.index(x)
-        for i, idx in enumerate(get_idxs(builder, idx_level[buf_index][level], buf_index)):
-          # first view is already implictly handled
-          idx, valid = self.sts[buf_index]._expr_idx(Variable(idx, 0, prod(self.sts[buf_index].shape)))
-          idx = idx.render(render_llvm, builder)
-          if valid.min == 0:
-            valid = valid.render(render_llvm, builder)
-            # this always does the load, so we have it load *0 if the arg won't be used
-            # TODO: would control flow be faster?
-            aug_idx = builder.select(valid, idx, int_const(0))
-            element = builder.select(valid, builder.load(builder.gep(func.args[buf_index], [aug_idx], inbounds=True)), ir.Constant(func_dtypes[buf_index], 0))
-          else:
-            element = builder.load(builder.gep(func.args[buf_index], [idx], inbounds=True))
-          # upcast
-          if func_dtypes[buf_index] != ir.FloatType(): element = builder.fpext(element, ir.FloatType())
-          m = element if kernel_output_dim == 1 else builder.insert_element(m, element, int_const(i))
-        return m
+        # first view is already implictly handled
+        idx, valid = self.sts[buf_index]._expr_idx(Variable(idx_level[buf_index][level], 0, prod(self.sts[buf_index].shape)))
+        idx = idx.render(render_llvm, builder)
+        if valid.min == 0:
+          valid = valid.render(render_llvm, builder)
+          # this always does the load, so we have it load *0 if the arg won't be used
+          # TODO: would control flow be faster?
+          aug_idx = builder.select(valid, idx, int_const(0))
+          element = builder.select(valid, builder.load(builder.gep(func.args[buf_index], [aug_idx], inbounds=True)), ir.Constant(func_dtypes[buf_index], 0))
+        else:
+          element = builder.load(builder.gep(func.args[buf_index], [idx], inbounds=True))
+        # upcast
+        if func_dtypes[buf_index] != ir.FloatType(): element = builder.fpext(element, ir.FloatType())
+        return element
       if isinstance(x.op, ReduceOps):
-        if reduce_result is None:
-          raise RuntimeError("no reduce")
+        if reduce_result is None: raise RuntimeError("no reduce")
         return reduce_result
       values = [ast_parse(builder, v, level, reduce_result) for v in x.src]
-
-      m = kernel_output_type(ir.Undefined)
-      if kernel_output_dim == 1:
-        return LLVMCodegen.op_lookup[x.op](builder, *values)
-      else:
-        # TODO: this only has to be done for certain ops
-        for i in range(kernel_output_dim):
-          value = [builder.extract_element(v, int_const(i)) for v in values]
-          element = LLVMCodegen.op_lookup[x.op](builder, *value)
-          m = builder.insert_element(m, element, int_const(i))
-        return m
+      return LLVMCodegen.op_lookup[x.op](builder, *values)
 
     # add the ast + final store
     store_loop = output_shape.index(1) if 1 in output_shape else -1
@@ -176,9 +107,8 @@ class LLVMCodegen(ASTKernel):
     if self.reduceop:
       reduce_input = ast_parse(loop_exit[-1], self.reduceop.src[0], -1)
       phis = [LLVMCodegen.start_for_op[self.reduceop.op]]  # type: ignore
-      if kernel_output_dim > 1: phis = [kernel_output_type(phis * kernel_output_dim)]
       for i in range(store_loop+1, len(loop_entry)):
-        val = loop_entry[i].phi(kernel_output_type, f"reduce_phi_{i}")
+        val = loop_entry[i].phi(ir.FloatType(), f"reduce_phi_{i}")
         val.add_incoming(phis[-1], loop_entry[i-1]._block)
         phis.append(val)
 
@@ -195,10 +125,9 @@ class LLVMCodegen(ASTKernel):
 
     # store result
     builder = loop_exit[store_loop]
-    for i, idx in enumerate(get_idxs(builder, idx_level[0][store_loop], 0)):
-      element = result if kernel_output_dim == 1 else builder.extract_element(result, int_const(i))
-      if func_dtypes[0] != ir.FloatType(): element = builder.fptrunc(element, func_dtypes[0])
-      builder.store(element, builder.gep(func.args[0], [idx], inbounds=True))
+    idx = idx_level[0][store_loop]
+    if func_dtypes[0] != ir.FloatType(): result = builder.fptrunc(result, func_dtypes[0])
+    builder.store(result, builder.gep(func.args[0], [idx], inbounds=True))
 
     # add the looping
     for i,s in enumerate(full_shape):
